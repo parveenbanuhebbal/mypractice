72b20e59efd14909626ee44cd81c3fb5ddc46f2a
Parveen <phebbal@innominds.com>
2013-07-22 02:32:45 +0530

    Testing the commit

---
+++ src/collection/test/TestArrayList.java
@@ -0,0 +1,11 @@
+package collection.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestArrayList {
+	public static void main(String[] args) {
+		List<String> myList = new ArrayList<String>();
+		myList.add("Hello");
+	}
+}
